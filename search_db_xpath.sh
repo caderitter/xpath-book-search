974bdd7f75ce37b97f21ff0bedb996109f5da164
Cade Ritter <ritter.cade@gmail.com>
2017-08-01 16:51:52 -0500

    Add setup

--- search_db_xpath.sh
+++ search_db_xpath.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+if [ "$1" == "setup" ]; then
+	if [ $# -ne 3 ]; then
+		echo $0: usage: search_db_xpath setup [username] [servername]
+	fi
+username=$2
+servername=$3
+
+connectionstring=$username@$servername.cnx.org
+echo "Copying SQL file to server..."
+scp search_db_xpath.sql $connectionstring:~/.
+echo "...done."
+echo "Creating functions on server..."
+ssh $connectionstring <<EOSSH || exit 1
+psql -U rhaptos -h /var/run/postgresql -d repository <<EOSQL
+\i search_db_xpath.sql
+\q
+EOSQL
+exit
+EOSSH
+echo "...done."
+exit
+fi
+
+
 if [ $# -ne 5 ]; then
     echo $0: usage: search_db_xpath [username] [servername] [xpath] [filename] [output-filename]
-    echo example usage: search_db_xpath cade qa \"//*[local-name=\'definition\']\" index.cnxml output.csv
     exit 1
 fi
 
